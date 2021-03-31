7059887db375d08559aa04c0f7f7be60ae942b2a
许向 <x2x4com@gmail.com>
2021-03-31 14:45:14 +0800

    dev update

--- filetest.py
+++ filetest.py
@@ -44,9 +44,16 @@ ac = api.Client("http://192.168.1.52:1633")
 # pprint(ac.get_files('52131e77eca88d99f8b96b7094ada330b9fdf0d480f05a4e70d7646a5fbf4180'))
 
 # 重复的文件与文件名可以多次添加，总返回第一次的哪个(文件不变的情况下)
-with open('muscle_doge.png', 'rb') as f:
-    pprint(ac.upload_files(file_name="muscle_doge.png", file_bin=f.read(), content_type='image/png'))
+# with open('muscle_doge.png', 'rb') as f:
+#     pprint(ac.upload_files(file_name="muscle_doge.png", file_bin=f.read(), content_type='image/png'))
     # 9cc8317473e3ed6cc5400cb290a4c3a3bc423a2d5f2877268971b13f01a1dbd1
     # 9cc8317473e3ed6cc5400cb290a4c3a3bc423a2d5f2877268971b13f01a1dbd1
 
+
+# tar文件上传测试
+with open('site.tar', 'rb') as t:
+    _b = t.read()
+    # print(_b)
+    pprint(ac.upload_collection(collection_bin=_b, timeout=1200))
+    # 2640678ea308036c080ceba64e0f23bef21baeba999614413d620ec2cb70d868
 
--- pyethswarm/api.py
+++ pyethswarm/api.py
@@ -14,7 +14,7 @@ class Client(ClientBase):
         super().__init__(url, self.__version)
         self.is_raise = is_raise
 
-    def create_bytes(self, swarm_tag: int, swarm_pin: bool, swarm_encrypt: bool):
+    def create_bytes(self, swarm_tag: int, swarm_pin: bool, swarm_encrypt: bool, **kwargs):
         method = "POST"
         path = "/bytes"
         headers = Headers({
@@ -24,19 +24,19 @@ class Client(ClientBase):
             "swarm-pin": swarm_pin,
             "swarm-encrypt": swarm_encrypt
         })
-        return self.call_request(method, path, headers=headers)
+        return self.call_request(method, path, headers=headers, **kwargs)
 
-    def get_bytes(self, reference: str):
+    def get_bytes(self, reference: str, **kwargs):
         method = "GET"
         path = "/bytes/%s" % ContentAddress(reference)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def get_chunks(self, reference: str):
+    def get_chunks(self, reference: str, **kwargs):
         method = "GET"
         path = "/chunks/%s" % ChunkAddress(reference)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_chunks(self, string_bin: bytes, swarm_tag: int, swarm_pin: bool):
+    def create_chunks(self, string_bin: bytes, swarm_tag: int, swarm_pin: bool, **kwargs):
         method = "POST"
         path = "/chunks"
         headers = Headers({
@@ -44,9 +44,9 @@ class Client(ClientBase):
             "swarm-pin": swarm_pin
         })
         headers['content-type'] = ContentType.OCTETSTEAM
-        return self.call_request(method, path, headers=headers, data=string_bin)
+        return self.call_request(method, path, headers=headers, data=string_bin, **kwargs)
 
-    def get_files(self, reference: str):
+    def get_files(self, reference: str, **kwargs):
         """
         Get file via reference(Content Address)
 
@@ -60,7 +60,7 @@ class Client(ClientBase):
         """
         method = "GET"
         path = "/files/%s" % ContentAddress(reference)
-        ret = self.call_request(method, path, stream=True)
+        ret = self.call_request(method, path, stream=True, **kwargs)
         if ret.code != 200:
             return ret
         _ret_data = ret.data
@@ -84,7 +84,25 @@ class Client(ClientBase):
                      file_bin: bytes,
                      swarm_tag: int = None,
                      swarm_pin: bool = False,
-                     swarm_encrypt: bool = False):
+                     swarm_encrypt: bool = False,
+                     **kwargs
+                     ):
+        """
+        Upload file,
+
+        Args:
+            file_name: file name
+            content_type:  file type
+            file_bin:  file byte
+            swarm_tag:  tag id
+            swarm_pin:  bool default false
+            swarm_encrypt: bool default false
+
+        Returns:
+            ok: return dict({"name": str, "reference": str})
+            error: RetType object
+
+        """
         method = "POST"
         path = "/files?%s" % urlencode({"name": file_name})
         if swarm_tag is not None:
@@ -101,7 +119,7 @@ class Client(ClientBase):
         # headers['content-type'] = "multipart/form-data"
         headers['content-type'] = content_type
         # print(headers)
-        ret = self.call_request(method, path, headers=headers, data=file_bin)
+        ret = self.call_request(method, path, headers=headers, data=file_bin, **kwargs)
         if ret.code != 200:
             return ret
         return {
@@ -111,25 +129,34 @@ class Client(ClientBase):
 
     def upload_collection(self,
                           collection_bin: bytes,
-                          swarm_tag: int,
-                          swarm_pin: bool,
-                          swarm_encrypt: bool,
-                          swarm_index_document: str,
-                          swarm_error_document: str
+                          swarm_tag: int = None,
+                          swarm_pin: bool = False,
+                          swarm_encrypt: bool = False,
+                          swarm_index_document: str = "index.html",
+                          swarm_error_document: str = "error.html",
+                          **kwargs
                           ):
         method = "POST"
         path = "/dirs"
-        headers = Headers({
-            "swarm-tag": swarm_tag,
-            "swarm-pin": swarm_pin,
-            "swarm-encrypt": swarm_encrypt,
-            "swarm-index-document": swarm_index_document,
-            "swarm-error-document": swarm_error_document
-        })
+        if swarm_tag is not None:
+            headers = Headers({
+                "swarm-tag": swarm_tag,
+                "swarm-pin": swarm_pin.__str__().lower(),
+                "swarm-encrypt": swarm_encrypt.__str__().lower(),
+                "swarm-index-document": swarm_index_document,
+                "swarm-error-document": swarm_error_document
+            })
+        else:
+            headers = Headers({
+                "swarm-pin": swarm_pin.__str__().lower(),
+                "swarm-encrypt": swarm_encrypt.__str__().lower(),
+                "swarm-index-document": swarm_index_document,
+                "swarm-error-document": swarm_error_document
+            })
         headers['content-type'] = ContentType.XTAR
-        return self.call_request(method, path, headers=headers, data=collection_bin)
+        return self.call_request(method, path, headers=headers, data=collection_bin, **kwargs)
 
-    def get_collection_index(self, reference: str, targets: str):
+    def get_collection_index(self, reference: str, targets: str, **kwargs):
         """
         https://docs.ethswarm.org/docs/advanced/persistence#global-pinning
 
@@ -139,22 +166,22 @@ class Client(ClientBase):
         """
         method = "GET"
         path = "/bzz/%s?%s" % (CollectionAddress(reference), urlencode({"targets": targets}))
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def get_collection_file(self, reference: str, file_path: str, targets: str):
+    def get_collection_file(self, reference: str, file_path: str, targets: str, **kwargs):
         method = "GET"
         path = "/bzz/%s/%s?%s" % (CollectionAddress(reference), file_path, urlencode({"targets": targets}))
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def get_tags(self, uid: str = None, offset: int = 0, limit: int = 0):
+    def get_tags(self, uid: str = None, offset: int = 0, limit: int = 0, **kwargs):
         method = "GET"
         if uid is not None:
             path = "/tags/%s" % uid
         else:
             path = "/tags?%s" % urlencode({"offset": offset, "limit": limit})
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_tag(self, peer_address: str):
+    def create_tag(self, peer_address: str, **kwargs):
         method = "POST"
         path = "/tags"
         data = {
@@ -162,14 +189,14 @@ class Client(ClientBase):
         }
         headers = Headers()
         headers['content-type'] = ContentType.JSON
-        return self.call_request(method, path, headers=headers, json=data)
+        return self.call_request(method, path, headers=headers, json=data, **kwargs)
 
-    def delete_tag(self, uid: int):
+    def delete_tag(self, uid: int, **kwargs):
         method = "DELETE"
         path = "/tags/%s" % uid
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def update_tag(self, uid: int, peer_address: str):
+    def update_tag(self, uid: int, peer_address: str, **kwargs):
         method = "PATCH"
         path = "/tags/%s" % uid
         data = {
@@ -177,81 +204,81 @@ class Client(ClientBase):
         }
         headers = Headers()
         headers['content-type'] = ContentType.JSON
-        return self.call_request(method, path, headers=headers, json=data)
+        return self.call_request(method, path, headers=headers, json=data, **kwargs)
 
-    def create_pin_chunk(self, chunk_address: str):
+    def create_pin_chunk(self, chunk_address: str, **kwargs):
         method = "POST"
         path = "/pin/chunks/%s" % ChunkAddress(chunk_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def get_pin_chunk(self, chunk_address: str = None, offset: int = 0, limit: int = 0):
+    def get_pin_chunk(self, chunk_address: str = None, offset: int = 0, limit: int = 0, **kwargs):
         method = "GET"
         if chunk_address is None:
             path = "/pin/chunks?%s" % urlencode({"offset": offset, "limit": limit})
         else:
             path = "/pin/chunks/%s" % ChunkAddress(chunk_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def update_pin_chunk(self, chunk_address: str):
+    def update_pin_chunk(self, chunk_address: str, **kwargs):
         method = "PUT"
         path = "/pin/chunks/%s" % ChunkAddress(chunk_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def delete_pin_chunk(self, chunk_address: str):
+    def delete_pin_chunk(self, chunk_address: str, **kwargs):
         method = "DELETE"
         path = "/pin/chunks/%s" % ChunkAddress(chunk_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_pin_bytes(self, bytes_address: str):
+    def create_pin_bytes(self, bytes_address: str, **kwargs):
         method = "POST"
         path = "/pin/bytes/%s" % BytesAddress(bytes_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def delete_pin_bytes(self, bytes_address: str):
+    def delete_pin_bytes(self, bytes_address: str, **kwargs):
         method = "DELETE"
         path = "/pin/bytes/%s" % BytesAddress(bytes_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_pin_files(self, file_address: str):
+    def create_pin_files(self, file_address: str, **kwargs):
         method = "POST"
         path = "/pin/files/%s" % FileAddress(file_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def delete_pin_files(self, file_address: str):
+    def delete_pin_files(self, file_address: str, **kwargs):
         method = "POST"
         path = "/pin/files/%s" % FileAddress(file_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_pin_collection(self, collection_address: str):
+    def create_pin_collection(self, collection_address: str, **kwargs):
         method = "POST"
         path = "/pin/bzz/%s" % CollectionAddress(collection_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def delete_pin_collection(self, collection_address: str):
+    def delete_pin_collection(self, collection_address: str, **kwargs):
         method = "POST"
         path = "/pin/bzz/%s" % CollectionAddress(collection_address)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def send_pss(self, topic: str, targets: str):
+    def send_pss(self, topic: str, targets: str, **kwargs):
         method = "POST"
         path = "/pss/send/{topic}/{targets}".format(topic=topic, targets=targets)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def subscribe_pss(self, topic: str):
+    def subscribe_pss(self, topic: str, **kwargs):
         method = "GET"
         path = "/pss/subscribe/{topic}".format(topic=topic)
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_single_owner_chunk(self, owner: str, _id: str, sig: str):
+    def create_single_owner_chunk(self, owner: str, _id: str, sig: str, **kwargs):
         method = "POST"
         path = "/soc/{owner}/{id}?{query}".format(
             owner=OwnerAddress(owner),
             id=IdAddress(_id),
             query=urlencode({"sig": sig})
         )
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
 
-    def create_feed(self, owner: str, topic: str, swarm_pin: bool, _type: str = "sequence"):
+    def create_feed(self, owner: str, topic: str, swarm_pin: bool, _type: str = "sequence", **kwargs):
         method = "POST"
         if _type not in ["sequence", "epoch"]:
             _type = "sequence"
@@ -263,9 +290,9 @@ class Client(ClientBase):
         headers = Headers({
             "swarm-pin": swarm_pin
         })
-        return self.call_request(method, path, headers=headers)
+        return self.call_request(method, path, headers=headers, **kwargs)
 
-    def get_feed(self, owner: str, topic: str, at: int = None, _type: str = "sequence"):
+    def get_feed(self, owner: str, topic: str, at: int = None, _type: str = "sequence", **kwargs):
         method = "GET"
         if _type not in ["sequence", "epoch"]:
             _type = "sequence"
@@ -276,4 +303,4 @@ class Client(ClientBase):
             topic=IdAddress(topic),
             query=urlencode({"type": _type, "at": at})
         )
-        return self.call_request(method, path)
+        return self.call_request(method, path, **kwargs)
